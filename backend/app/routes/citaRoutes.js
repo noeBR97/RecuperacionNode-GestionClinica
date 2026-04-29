62a636e527772682cd78d5c6fc7252977a442ded
Noelia Barrionuevo <noe.br97@gmail.com>
2026-04-29 11:30:04 +0200

    rutas citas

---
+++ backend/app/routes/citaRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import { crearCita, getCitas, buscarCita, actualizarCita, eliminarCita } from '../controllers/citaController.js'
+import { validarJWT } from '../middlewares/validarJWT.js'
+import { esAdmin } from '../middlewares/validarRol.js'
+
+const router = Router()
+
+router.use(validarJWT)
+router.use(esAdmin)
+
+router.get('/', getCitas)
+router.get('/:id', buscarCita)
+router.post('/', crearCita)
+router.put('/:id', actualizarCita)
+router.delete('/:id', eliminarCita)
